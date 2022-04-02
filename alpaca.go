12bb57c3c970eb36a2ab64cee32b65a82ed45907
Suraj Dalsania <dalsania612@gmail.com>
2022-04-02 18:43:15 -0400

    initial setup of repo

---
+++ alpaca.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"log"
+	"github.com/shopspring/decimal"
+	"os"
+	"fmt"
+	"github.com/alpacahq/alpaca-trade-api-go/v2/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/v2/marketdata"
+	"github.com/aws/aws-lambda-go/lambda"
+	
+
+	"context"
+)
+var APCA_API_KEY_ID = os.Getenv("APCA_API_KEY_ID")
+var APCA_API_SECRET_KEY = os.Getenv("APCA_API_SECRET_KEY")
+
+
+func createOrder(not decimal.Decimal, symbol string) alpaca.PlaceOrderRequest{
+	  var marketOrder = alpaca.PlaceOrderRequest{
+	   AssetKey: &symbol,
+	   Notional:&not,
+	   Side:"buy",
+	   Type:"market",
+	   TimeInForce:"day",
+	   }
+	   return marketOrder
+}
+func HandleRequest(ctx context.Context) {
+
+	marketClient := marketdata.NewClient(marketdata.ClientOpts{
+		ApiKey:    APCA_API_KEY_ID,
+		ApiSecret: APCA_API_SECRET_KEY,
+		BaseURL: "https://data.alpaca.markets",
+	})
+	alpacaClient := alpaca.NewClient(alpaca.ClientOpts{
+		ApiKey:    APCA_API_KEY_ID,
+		ApiSecret: APCA_API_SECRET_KEY,
+		BaseURL: "https://paper-api.alpaca.markets",
+		})
+
+	 var not decimal.Decimal
+	 snapshot,err:= marketClient.GetSnapshot("VOO")
+
+	 if err != nil {
+		 log.Printf("Failed to retrieve Snapshot Data: %p",snapshot)
+	 }
+
+
+	 var prevDailyClose = snapshot.PrevDailyBar.Close
+	 latestTrade,err := marketClient.GetLatestTrade("VOO")
+	 if err != nil{
+		 log.Printf("Failed to get latetest trade for stock: %p", err)
+	 }
+	 var lastTradePrice = latestTrade.Price
+
+	 fmt.Printf("Previous Daily Close: %f \n", prevDailyClose)
+	 fmt.Printf("Last Trading Price of stock %f \n",lastTradePrice)
+	 
+	 var diff = lastTradePrice - prevDailyClose
+	 fmt.Printf("The diff between the latest trade of the stock and yesterdays close: %f \n",diff)
+	 var perChange = diff/lastTradePrice
+	 fmt.Printf("The percent change: %f \n",perChange)
+	
+	if perChange < 0.02 && perChange > -0.015{
+		fmt.Println("-0.015 to 0.020: buy 4 dollars")
+		not = decimal.NewFromInt(1)
+	}
+	if perChange > -0.015 && perChange < -0.02 {
+		fmt.Println("-0.015 to -0.020: buy 8 dollars")
+		not = decimal.NewFromInt(4)
+	}
+	if perChange > -0.02 && perChange < -0.04 {
+		fmt.Println("-0.02 to -0.04: buy 10 dollars")
+		not = decimal.NewFromInt(8)
+	}
+	if perChange < -0.05  {
+		fmt.Println("losses greater than -0.05: buy 12 dollars")
+		not = decimal.NewFromInt(12)
+	}
+	orderStruct := createOrder(not, "VOO")
+	_,err = alpacaClient.PlaceOrder(orderStruct)
+	if err != nil{
+		log.Printf("Failed to place order: %s", err)
+	}
+
+	}
+
+func main() {
+	lambda.Start(HandleRequest)
+	// fmt.Printf("%+v\n", *acct)
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module lambda-go
+
+go 1.17
+
+require (
+	cloud.google.com/go v0.99.0 // indirect
+	github.com/alpacahq/alpaca-trade-api-go/v2 v2.3.0 // indirect
+	github.com/aws/aws-lambda-go v1.28.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/klauspost/compress v1.11.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.0 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
